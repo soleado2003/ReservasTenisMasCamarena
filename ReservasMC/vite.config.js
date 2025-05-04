6f4373ec615406133f05614ee93ef915841c0290
Damian Martinez <damarchu@hotmail.com>
2025-05-05 01:56:30 +0200

    c

--- ReservasMC/vite.config.js
+++ ReservasMC/vite.config.js
@@ -4,16 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/ReservasMC',
-  server: {
-
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3010',
-        changeOrigin: true,
-        secure: false,
-
-      }
-    }
-  },
+  base: '/',
   plugins: [react()],
 })
